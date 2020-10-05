88c8f5427e19f67b7339194ef21f09fb1a5c4d31
Lucien Gaitskell <lucieng@gaitskell.com>
2020-10-05 09:46:48 -0400

    Add data assignment 3

---
+++ 3-data/athome3_sorting_algos.py
@@ -0,0 +1,34 @@
+"""
+Sorting Algos Handout  
+Due: October 7th, 2020 
+
+NOTES: 
+
+- Reference class notes here: https://docs.google.com/document/d/1AWM3nnLc-d-4nYobBRuBTMuCjYWhJMAUx2ipdQ5E77g/edit?usp=sharing
+- Do NOT use online resources. 
+- If you are stuck or want help on any of them, come to office hours!  
+
+Completed by Lucien Gaitskell (LUC)
+
+
+EXERCISES 
+
+1. Suppose we have 243 identical-looking boxes each containing a drone ready to ship, but we remember that one box is
+missing instructions! This box will weigh a little less than the rest and there is a scale that can compare weights of any
+two sets of boxes. Assuming the worst case scenario, what is the minimum number of weightings needed to determine
+which box has the missing instructions?
+
+Hint: review the Tower of Hanoi stuff from class
+
+2. Implement the following sorting algos (Insertion Sort, Merge Sort, and Quick Sort) in Python. Use comments to denotate which pieces of
+code refer to the recursive process of divide, conquer, and combine. Include 3-5 test cases for each algo.
+
+Hint: review the pseudocode from the lecture notes
+
+3. In terms of time complexity, which one is more efficent - Merge Sort or Quick Sort? What about space memory efficency?
+
+
+"""
+
+
+
