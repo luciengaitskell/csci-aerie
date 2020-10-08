e25ec033f056d71bea5b8cef8c0c9e4c75438d6a
Lucien Gaitskell <lucieng@gaitskell.com>
2020-10-08 14:49:48 -0400

    Add data assignment 4

---
+++ 3-data/athome4_stacks_queues.py
@@ -0,0 +1,37 @@
+"""
+Stacks and Queues Handout 
+Due: Tuesday, October 13th 
+
+NOTES: 
+
+- For the exercises, please do NOT use online resources. They are common algorithm questions so
+    they have answers online but it won't help your understanding.
+- If you are stuck or want help on any of them, come to office hours! 
+
+Completed by Lucien Gaitskell (LUC)
+
+"""
+
+
+## Exercises
+
+# 1.  Describe how you could use a single array to implement three stacks
+
+# 2. How would you design a stack which, in addition to push and pop,
+#       has a function min which returns the minimum element?
+
+# 3. In reference to question 2, what is the run time of all of these three operations?
+
+# 4.  Implement a MyQueue class which implements a queue using two stacks
+
+# 5.  Write a program to sort a stack such that the smallest items are on the top. You can use
+#       an additional temporary stack, but you may not copy the elements into any other data structure
+#       (such as an array). The stack supports the following operations: push, pop, peek, and isEmpty.
+
+# 6.  Remember this question from the arrays homework?
+#       "Given an array of integers and an integer k, you need to find the total number of continuous
+#       subarrays whose sum equals to k."
+#
+#       With you new understanding of how hashtables work, implement a solution to this problem using a dictionary.
+
+
