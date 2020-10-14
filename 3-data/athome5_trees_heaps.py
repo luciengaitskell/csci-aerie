2bf315661f55724edaf566d32eb0501be14939b4
Lucien Gaitskell <lucieng@gaitskell.com>
2020-10-14 12:32:25 -0400

    Add data assignment 5 (trees/heaps)

---
+++ 3-data/athome5_trees_heaps.py
@@ -0,0 +1,42 @@
+"""
+Trees and Heaps Handout 
+Due: Tuesday, October 20th 
+
+NOTES: 
+
+- For the exercises, please do NOT use online resources. They are common algorithm questions so
+    they have answers online but it won't help your understanding.
+- If you are stuck or want help on any of them, come to office hours! 
+
+Completed by Lucien Gaitskell (LUC)
+
+"""
+
+"""
+Exercises
+
+    1. Using the node class we created in class, implement functions to represent the 
+        3 different orders  you can use to recursively conduct DFS 
+        (each order should have a function).
+
+    2. Implement a priority queue simply using list. PQ class should include functions 
+        that check if the PQ is empty, adds an element to the PQ, and finally removes 
+        an element. 
+
+        Reminder: Most of the logic in terms of how a PQ works is in how it REMOVES 
+        an element (highest priority element is dequeued).
+
+    3. Now implement a PQ using a linked list. 
+
+    4. Implement a min-heap class demonstrating common heap functions. Your class should include functions 
+        that initialize a min-heap using a list, insert a new node, decrease the value of node at index 'i' 
+        to new value (with new value being less than old value), remove the min node, deleting a node
+        at index "i", and finally returning the minimum value. Feel free to include more functions if you wish.
+
+        Hint: Utilize built-in functions (heappush, heappop, heapify) from module heapq.  
+
+    5. Time complexity - what is the time complexity of a PQ implemented as a linked list vs a binary heap? 
+
+
+"""
+
