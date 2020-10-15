11e9441b76027e41c146a33549adcb6f4694afd0
Lucien Gaitskell <lucieng@gaitskell.com>
2020-10-15 17:11:40 -0400

    Add stacks and queues #6 response

--- 3-data/athome4_stacks_queues.py
+++ 3-data/athome4_stacks_queues.py
@@ -146,9 +146,44 @@ assert stack1.get() == 9
 #       subarrays whose sum equals to k."
 #
 #       With you new understanding of how hashtables work, implement a solution to this problem using a dictionary.
+import math
 
-""" LUC - Need help
- 
-I do not understand how hash tables could be applied in this situation.
 
-"""
+def subarrays(l, k):
+    """
+    LUC
+
+    :param l: List to search through
+    :param k: Subarray count target
+    :return: Number of subarray combinations that add to k
+    """
+
+    # Sum count data
+    sums = {}
+    # structure: {sum: count}
+    #   NOTE: count will mostly be duplicate by the end
+    #       except for single element sub arrays
+    #       THEREFORE: Have to divide by 2 and take ceil
+
+    for val in l:   # Iterate through all values
+        new_sums = {}   # New calculated sum counts from this value `val`
+        for s, s_val in sums.items():   # Iterate through all previous sum/count pairs
+            new_s = s+val   # Calculate new sum from existing sum plus this value
+            new_sums[new_s] = sums.get(new_s, 0) + (sums[s] + 1)
+            #   Increment new sum count (starting from 0 if not yet set)
+            #       and add previous sum `s` counts plus 1
+
+        sums.update(new_sums)  # Update sum structure with these changes / new values
+
+        sums[val] = sums.get(val, 0) + 1  # Also include this value as single set in sum count
+
+    return math.ceil(sums.get(k, 0)/2)  # Get sums for size k: reference note on `sums`
+
+
+assert subarrays([1, 1, 2, 3, 4], 4) == 4
+assert subarrays([1, 2, 3, 4], 4) == 2
+assert subarrays([], 4) == 0
+assert subarrays([2], 4) == 0
+assert subarrays([4], 4) == 1
+assert subarrays([1,4,5,10], 10) == 2
+assert subarrays([1,4,5,5,10], 10) == 4
