a36e779e16050a99aa6d83102722d0874e3a6a4f
Lucien Gaitskell <lucieng@gaitskell.com>
2021-02-15 18:19:00 -0500

    Add header to CSCI1470 assignment 1

--- csci1470/assignment1/assignment.py
+++ csci1470/assignment1/assignment.py
@@ -1,3 +1,10 @@
+""" CSCI1470 Assignment 1
+
+Response by Lucien Gaitskell
+
+References:
+- https://brown-deep-learning.github.io/dl-website-2020/projects/public/hw1-mnist-numpy/hw1-mnist-numpy.html
+"""
 import numpy as np
 from pathlib import Path
 import requests
